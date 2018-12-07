59da559d1f497c61fef8f81936cd98832e4df940
itacirgabral <itacirgabral@users.noreply.github.com>
2018-12-07 20:23:34 -0300

    keys all day

--- src/App.js
+++ src/App.js
@@ -1,28 +1,58 @@
 import { useState, createElement} from 'react'
 import './App.css'
 
+function Item(e, i) {
+  return createElement('li', {className: 'todo-list', key: e.id}, e.text)
+}
+
+function Heading() {
+  return createElement('h1', {key: 'heading'}, 'ToDo')
+}
+
+function Input({onChange, onClick}) {
+  return createElement('div', {className: 'input', key: 'Input'}, [
+    createElement('input', {type: 'text', onChange, key: 'fild'}),
+    createElement('button', {type: 'button', key: 'add', onClick}, 'add'),
+    createElement('br', {key: 'br'}),
+  ])  
+}
+
+
+const rChar = () => String.fromCharCode(Math.trunc(Math.random()*26) + 97)
+const mkKeyGen = arr => () => {
+  let key
+  while(arr.includes(key = rChar() + rChar() + rChar() + rChar())){
+    
+  }
+  return key
+}
+const keyGen = mkKeyGen(['avde', 'dcrg', 'otcj'])
+
+/*   __   ____  ____ 
+**  / _\ (  _ \(  _ \
+** /    \ ) __/ ) __/
+** \_/\_/(__)  (__)  
+*/
 function App() {
   const [todo, setTodo] = useState([
-    { text: "ToDo 1"},
-    { text: "ToDo 2" },
-    { text: "ToDo 3" }
+    { text: 'ToDo 1', id: 'avde'},
+    { text: 'ToDo 2', id: 'dcrg'},
+    { text: 'ToDo 3', id: 'otcj'}
   ])
-
-  const [input, setInput] = useState("")
-
   const todoPush = e => {
-    setTodo([...todo, {text: input}])
+    setTodo([...todo, {text: input, id: keyGen}])
   }
 
-  return createElement('div', null, [
-    createElement('h1', {key: 'heading'}, 'ToDo'),
-    createElement('input', {type: 'text', key: 'inputText', onChange: e => setInput(e.target.value)}),
-    createElement('button', {type: 'button', key: 'add', onClick: todoPush}, 'add'),
-    createElement('br', {key: 'br'}),
+  const [input, setInput] = useState("")
+  const inputChange = e => setInput(e.target.value)
+
+  return createElement('div', {className: 'app'}, [
+    Heading(),
+    Input({onChange: inputChange, onClick: todoPush}),
     createElement('ul', {className: 'todo', key: 'list'}, 
-      todo.map((e, i) => createElement('li', {className: 'todo-list', key: e.id}, e.text))
+      todo.map(Item)
     )
   ])
 }
 
-export default App
+export default createElement(App)
--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
-import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(React.createElement(App), document.getElementById('root'));
+ReactDOM.render(App, document.getElementById('root'));
 
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
 serviceWorker.unregister();
+
+// sudo sysctl fs.inotify.max_user_watches=524288
+// sudo sysctl -p
